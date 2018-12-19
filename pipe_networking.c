4b7a5fe340b4c6d518baf8bdb124dd78a71f09f9
Maia Brydon <mbrydon@stuy.edu>
2018-12-19 15:27:50 -0500

    fixed handshake

--- pipe_networking.c
+++ pipe_networking.c
@@ -12,28 +12,29 @@ returns the file descriptor for the upstream pipe.
 =========================*/
 int server_handshake(int *to_client) {
   // create and open well-known pipe
-  char * wk_pipe = "/tmp/wk_pipe";
+  char * wk_pipe = "wk_pipe";
   printf("creating well-known pipe\n");
   mkfifo(wk_pipe, 0666);
   int fd_wkp = open(wk_pipe, O_RDONLY);
   // read from well-known pipe
   char * pp = malloc(10);
   int bytes_read = read(fd_wkp, pp, 100);
-  // connect to private pipe
-  char p_pipe[20] = "/tmp";
-  strcat(p_pipe, pp);
-  int fd_pp = open(p_pipe, O_WRONLY);
-  // print message recieved + send message
+  // print message recieved + return the message
   if(bytes_read){
     printf("from client: [%s]\n", pp);
-    if(strcmp(pp, ACK)){
-      write(fd_pp, pp, 10);
-    }
+    int fd_pp = open(pp, O_WRONLY);
+    to_client = &fd_pp;
+    write(fd_pp, pp, 10);
   }
-  // close pipes
-  close(fd_pp);
-  close(fd_wkp);
-  to_client = &fd_pp;
+  // remove well-known pipe
+  printf("removing well-known pipe\n");
+  remove(wk_pipe);
+  // print confirmation message from client
+  char * msg = malloc(10);
+  int msg_read = read(fd_wkp, msg, 100);
+  if(msg_read){
+    printf("from client: [%s]\n", msg);
+  }
   return fd_wkp;
 }
 
@@ -49,29 +50,25 @@ returns the file descriptor for the downstream pipe.
 =========================*/
 int client_handshake(int *to_server) {
   // create and open private pipe
-  // PID DOES NOT TRANSLATE PROPERLY FROM CHAR TO INT
-  // FIX SO PROPER PID IS SENT TO SERVER
-  char pid = getpid();
-  char p_pipe[20] = "/tmp";
-  strcat(p_pipe, &pid);
-//  printf("%c\n", pid);
+  char * p_pipe = malloc(10);
+  sprintf(p_pipe, "%d", getpid());
   printf("creating private pipe\n");
   mkfifo(p_pipe, 0666);
-  int fd_pp = open(p_pipe, O_RDONLY);
   // open well-known pipe
-  char * wk_pipe = "/tmp/wk_pipe";
+  char * wk_pipe = "wk_pipe";
   int fd_wkp = open(wk_pipe, O_WRONLY);
   // write pid to well-known pipe
-  int bytes_written = write(fd_wkp, &pid, 10);
+  int bytes_written = write(fd_wkp, p_pipe, 10);
+  // open private pipe and print message recieved
+  int fd_pp = open(p_pipe, O_RDONLY);
   char pp[10];
   int bytes_read = read(fd_pp, pp, 10);
   if(bytes_read){
     printf("from server: [%s]\n", pp);
-    write(fd_wkp, ACK, 10);
+    printf("removing private pipe\n");
+    remove(p_pipe);
+    write(fd_wkp, ACK, 10); // write confirmation message to server
   }
-  // close pipes
-  close(fd_pp);
-  close(fd_wkp);
   to_server = &fd_wkp;
   return fd_pp;
 }
